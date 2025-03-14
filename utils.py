a10f64f835de9a96ac4717d755a0c794f9a50e51
Elian Hernandez <105662967+1Eliaaaan@users.noreply.github.com>
2025-03-14 01:30:03 -0500

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def initialize_session_state():
-    """Inicializa las variables de estado de la sesión"""
-    if 'logged_in' not in st.session_state:
-        st.session_state.logged_in = False
-    
-    if 'username' not in st.session_state:
-        st.session_state.username = None
